da89d4351ffb5077dcbbed4f7ab16a1e3219778d
GS05-44-MatsumotoDaisuke <da.mats1990@gmail.com>
2017-07-10 00:28:51 +0900

    進む・戻る、再生・停止ボタンを追加

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,16 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  
+  @IBOutlet weak var backGround: UIImageView!
+  @IBOutlet weak var mainPhoto: UIImageView!
+  
+  
+  @IBOutlet weak var nextButton: UIButton!
+  @IBOutlet weak var backButton: UIButton!
+  
+  
+  //メイン画像
+  let photos = ["summer_01.png","summer_02.png","summer_03.png"]
+  //背景画像
+  let backgrounds = ["bg_01.png","bg_02.png","bg_03.png"]
+  
+  //タイマー用変数
+  var timer: Timer!
+  var timer_sec: Float = 0
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //メイン画像
+    mainPhoto.image = UIImage(named:"summer_01.png")
+    
+    //背景画像
+    backGround.image = UIImage(named:"bg_01.png")
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  //進むボタン
+  var photonumber = 0
+  @IBAction func next(_ sender: Any) {
+    photonumber += 1
+    
+    if photonumber > 2{
+      photonumber = 0
+    }
+    mainPhoto.image = UIImage(named:photos[photonumber])
+    backGround.image = UIImage(named:backgrounds[photonumber])
+    
+    
+  }
+  
+  //戻るボタン
+  @IBAction func back(_ sender: Any) {
+    if photonumber == 0 {
+      photonumber = 2
+    } else{
+      photonumber -= 1
+    }
+    mainPhoto.image = UIImage(named:photos[photonumber])
+    backGround.image = UIImage(named:backgrounds[photonumber])
+  }
+  
+  //タイマー関数
+  func updateTimer(timer: Timer){
+    self.timer_sec += 2
+    photonumber += 1
+    
+    if photonumber > 2{
+      photonumber = 0
+    }
+    mainPhoto.image = UIImage(named:photos[photonumber])
+    backGround.image = UIImage(named:backgrounds[photonumber])
+  }
+  
+  //再生・停止ボタン
+  @IBAction func goAndStop(_ sender: Any) {
+    if self.timer == nil{
+      self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+      nextButton.isEnabled = false
+      backButton.isEnabled = false
+    } else{
+      self.timer.invalidate()
+      self.timer = nil
+      nextButton.isEnabled = true
+      backButton.isEnabled = true
+    }
+  }
+  
 
 
 }
